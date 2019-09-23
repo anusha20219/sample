090819ff7010514bccaff5294d0a52e65f14db43
anusha20219 <53473615+anusha20219@users.noreply.github.com>
2019-09-23 16:34:28 +0530

    Update program 9.c

--- program 9.c
+++ program 9.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
- 
-int n, k;
+ int n, k;
 int arr[100005];
 int temp[100005];
 int answer = 0;
 int sum = 0;
 int a = 0; 
 int b = 0; 
-int main() {
+int main() 
+{
 	scanf("%d %d", &n, &k);
-	for (int i = 1; i <= n; i++) {
+	for (int i = 1; i <= n; i++) 
+	{
 		char bulb;
 		scanf(" %c", &bulb);
 		if (bulb == 'G') arr[i] = 0;
@@ -17,14 +18,17 @@ int main() {
 		if (bulb == 'Y') arr[i] = 2;
 	}
  
-	for (int i = 1; i<=n; i++) {
+	for (int i = 1; i<=n; i++) 
+	{
 		arr[i] += sum;
-		if ((arr[i] + 1) % 3 == 0) {
+		if ((arr[i] + 1) % 3 == 0) 
+		{
 			temp[i] = 1;
 			answer+=1;
 			sum+=1;
 		}
-		else if ((arr[i] + 2) % 3 == 0) {
+		else if ((arr[i] + 2) % 3 == 0)
+		{
 			temp[i] = 2;
 			answer += 2;
 			sum += 2;
